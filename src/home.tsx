f4234951252865c3f781eec4151e0e14068cbb40
isaac long <isaaclong123@outlook.com>
2022-07-05 19:19:15 +0000

    switching gears

--- src/home.tsx
+++ src/home.tsx
@@ -1,66 +1,53 @@
-import React from "react";
+import React,{useState, useRef} from "react";
 import styled, { keyframes } from "styled-components";
 import {fadeIn} from "react-animations"
-
 import Login from "./login"
 const fadeInAni = keyframes`${fadeIn}`;
 
+
 const HomeDiv = styled.div`
-
-
-
-
-`
-const Page1 = styled.div`
-background: linear-gradient(to top, #0077B6, #caf0f8);
+width: 100vw;
+height:100vh;
 
 `
-const Page2 = styled.div`
-background: linear-gradient(180deg, hsla(201, 100%, 36%, 1) 0%, hsla(235, 100%, 12%, 1) 100%);
 
-`
-const Page3 = styled.div`
-background: linear-gradient(180deg, hsla(235, 100%, 12%, 1) 0%, hsla(0, 0%, 0%, 1) 100%);
 
-`
-// width: 300px;
-// 	height: 40vh;
-//     background-color: rgba(255, 255, 255, .15);
-  
-//     backdrop-filter: blur(5px);pointer-events:none;	margin: auto;
-//     margin-top: 30vh;
-//     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-// 	border-radius: 6px;
-
-const Zinger = styled.h1`
-text-align: center;
-margin-top:40vh;
-color: rgba(255, 255, 255, .3);
-font-size: 10rem;
-
-`
 const Home = ()=>{
+const colors = ["264653", "2a9d8f", "e9c46a", "f4a261", "e76f51"]
+const [colorI, setColorI]= useState(0)
+const [color, setColor] = useState(colors[0]);
+const [color2, setColor2] = useState(colors[1]);
+
+const nextScene = ()=>{
+    setColor(colors[colorI]);
+    setColor2(colors[colorI +1 ])
+    if(colorI < colors.length-1){
+            setColorI(colorI + 1)
+
+    }
+    else{
+        setColorI(0)
+    }
+
+}
 
 
     return(
-        <HomeDiv>
-            <Page1 className="page container">
-                <div className="row">
+        <HomeDiv style={
+            {backgroundColor: `#${color}`,
+            transition: "all 1.3s ease",
+            WebkitTransition: "all 1.3s ease",
+            MozTransition:"all 1.3s ease"   
+            
+            }}>
+            <Login color1={color} color2={color2} step={nextScene} />
+             
+           
 
-                    <Login></Login>
-
-                </div>
-            </Page1>
-
-            <Page2 className="page">
-
-            </Page2>
-
-            <Page3 className="page">
-
-            </Page3>
         </HomeDiv>
+        
     )
+    
 }
 
 export default Home;
--- src/login.tsx
+++ src/login.tsx
@@ -6,16 +6,79 @@ import {fadeIn} from "react-animations"
 const fadeInAni = keyframes`${fadeIn}`;
 
 const Container = styled.div`
-background-color: orange;
-height: 100px;
+position: fixed;
+pointer-events:auto;
+@media (min-width: 768px){
+    width: 50vw;
+    margin-left: 25vw;
+}
+
+@media (min-width:  1000px){
+    width: 30vw;
+    margin-left: 35vw;
+}
+height: 50vh;
+width: 75vw;
+max-width:100%;
+
+margin-left: 12.5vw;
+margin-top: 25vh;
+border-radius: 15px;
+background-color: rgba(255, 255, 255, .15);
+  
+  backdrop-filter: blur(5px);pointer-events:none;
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, .2); 
+
+`
+const LoginBtn = styled.button`
+    width: 100%;
+    border: none;
+    border-radius: 3px;
+    color: white;
+    pointer-events:auto;
+    transition: "all 1.3s ease",
+    Webkit-transition: "all 1.3s ease",
+    Moz-transition:"all 1.3s ease"
+
+`
+const HomeDiv = styled.div`
+width: 100vw;
+height:100vh;
 
 `
 
-const Login = ()=>{
+const TextField = styled.input`
+width: 100%;
+border: none;
+pointer-events:auto;
 
+`
+interface LoginProps {
+  color1: String,
+  color2: String,
+  step: Function 
+}
+
+const Login = (props: LoginProps)=>{
+
+  var color1 = props.color1;
+  var color2 = props.color2;
+  var step = props.step;
 
     return(
-       <Container className="col-lg-4 mx-auto"></Container>
+       <Container >
+   <div className="row">
+
+<div className="col-lg-4">
+    <TextField placeholder="Username" onChange={e => setUsername(e.target.value)}></TextField>
+    <LoginBtn style={{backgroundColor: `#${color2}`}} onClick={function(){step()}}>sup</LoginBtn>
+
+
+</div>
+
+</div>
+        
+       </Container>
     )
 }
 
